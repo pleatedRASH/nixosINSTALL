77e13bb79803b23f932e192f995a101b5e8dbda5
pleatedRASH <110006508+pleatedRASH@users.noreply.github.com>
2022-09-07 03:08:44 -0400

    Create hm_base.nix

---
+++ hm_base.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS configuration and Home Manager configuration";  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };  outputs = { home-manager, nixpkgs, ... }: {
+    nixosConfigurations = {
+      nixtst = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.mudrii = import ./users/$USER/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
