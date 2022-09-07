d52c111635fd7ea37a921aca871b3036e8aa27e3
pleatedRASH <110006508+pleatedRASH@users.noreply.github.com>
2022-09-07 03:06:04 -0400

    Create base.nix

---
+++ base.nix
@@ -0,0 +1,14 @@
+{
+  description = "NixOS configuration";  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+  };  outputs = { nixpkgs, ... }: {
+    nixosConfigurations = {
+      nixtst = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
